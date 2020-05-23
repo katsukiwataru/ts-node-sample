2bfdf145554af1f802b72c54fc97f8df3c270374
Wataru Katsuki <a17dc050@dhw.ac.jp>
2020-05-24 00:35:09 +0900

    feat: add api

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,27 @@
-function hello(name: string): string {
-  return `Hello ${name}`
-}
+import express from 'express';
 
-console.log(hello("World"));
+const app: express.Express = express();
+
+app.use((req: express.Request, res: express.Response, next) => {
+  res.header('Acess-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const router: express.Router = express.Router();
+router.get('/api/getTest', (req: express.Request, res: express.Response) => {
+  res.send(req.query);
+});
+router.post('/api/postTest', (req: express.Request, res: express.Response) => {
+  res.send(req.body);
+});
+
+app.use(router);
+
+app.listen(3000, () => {
+  // eslint-disable-next-line no-console
+  console.log('Example app listening on port 3000!');
+});
